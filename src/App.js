0935ac0981efaa848e78bdd845b539dd304d8e11
sanderjson <sanderson.jma@gmail.com>
2020-02-07 18:31:31 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getResults();
+  }, []);
+
+  const getResults = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/api/hello");
+      console.log(response);
+
+      setResults(response);
+    } catch (err) {
+      setError(err);
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ color: "red" }}>This is the header</h1>
+      {loading ? <div>Loading Results ...</div> : <div>{results}</div>}
+      {error && <div>{error.message}</div>}
     </div>
   );
 }
-
-export default App;
